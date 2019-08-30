5b931637e1fb77bdb2bd45c183536bf33e310f99
Alejandro Radzievicius <aleradzi@e1z3r2p17.42.us.org>
2019-08-30 13:56:36 -0700

    p

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: aleradzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/29 15:48:31 by aleradzi          #+#    #+#             */
-/*   Updated: 2019/08/30 11:38:58 by aleradzi         ###   ########.fr       */
+/*   Updated: 2019/08/30 13:56:17 by aleradzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	*tdest;
 	char	*tsrc;
-	size_t	i;
 
-	i = -1;
 	tsrc = (char *)src;
 	tdest = (char *)dest;
 	if (tsrc < tdest)
-		while ((int)(--n) >= 0)
-			*(tdest + n) = *(tsrc + n);
+		while (n)
+		{
+			*(tdest + n - 1) = *(tsrc + n - 1);
+			n--;
+		}
 	else
-		while (++i < n)
-			*(tdest + i) = *(tsrc + i);
+		ft_memcpy(tdest, tsrc, n);
 	return (dest);
 }
