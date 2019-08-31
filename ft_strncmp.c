1e7a678fd098361637be0a5c0de2c92920fe82b1
Alejandro Radzievicius <aleradzi@e1z3r2p17.42.us.org>
2019-08-30 17:34:01 -0700

    p

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,25 +6,21 @@
 /*   By: aleradzi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/24 16:51:03 by aleradzi          #+#    #+#             */
-/*   Updated: 2019/08/27 18:29:52 by aleradzi         ###   ########.fr       */
+/*   Updated: 2019/08/30 17:33:43 by aleradzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *str1, const char *str2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int		i;
+	size_t	i;
 
 	i = 0;
-	if (!n)
+	while (s1[i] && s2[i] && (unsigned char)s1[i] == (unsigned char)s2[i]
+			&& i < n)
+		i++;
+	if (i == n)
 		return (0);
-	while (n--)
-	{
-		if (str1[i] != str2[i])
-			break ;
-		if (n && str1[i] && str2[i])
-			i++;
-	}
-	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
